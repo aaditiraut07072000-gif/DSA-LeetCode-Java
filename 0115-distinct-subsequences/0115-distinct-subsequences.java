e454317dd82aaa716f34b6aafcfa1c30e90e1e88
aaditiraut07072000-gif <aaditiraut07072000@gmail.com>
2026-09-06 23:48:09 +0530

    Time: 22 ms (24.24%), Space: 42.3 MB (99.77%) - LeetHub

---
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int m = s.length();
+        int n = t.length();
+
+        // dp[j] = number of ways to form t[0...j-1]
+        int[] dp = new int[n + 1];
+
+        // Empty string can be formed in exactly 1 way
+        dp[0] = 1;
+
+        for (int i = 1; i <= m; i++) {
+            // Traverse backwards so previous values are not overwritten
+            for (int j = n; j >= 1; j--) {
+
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[j] = dp[j] + dp[j - 1];
+                }
+            }
+        }
+
+        return dp[n];
+    }
+}
+
